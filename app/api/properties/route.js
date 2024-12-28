695cd9b96c38b80785eabdcf8468ff3a8dc49dc6
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-12-28 14:49:47 +0700

    Added cors headers.

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -2,15 +2,29 @@ import connectMongodb from "../../../lib/mongodb";
 import propertyModel from "../../../models/propertyModel";
 import { NextResponse } from "next/server";
 
+function addCorsHeaders(response) {
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  return response;
+}
+
 export async function POST(req) {
   try {
     const body = await req.json();
     await connectMongodb();
     const listing = await propertyModel.create(body);
-    return NextResponse.json(
+    const response = NextResponse.json(
       { message: "Property created successfully", listing },
       { status: 201 }
     );
+    return addCorsHeaders(response);
   } catch (error) {
     console.log(error);
     return NextResponse.json(
@@ -21,13 +35,22 @@ export async function POST(req) {
 }
 
 // Fetch all properties
-export async function GET() {
+
+export async function GET(req) {
   try {
+    console.log("GET request received, attempting to connect to MongoDB...");
+
     await connectMongodb();
+    console.log("MongoDB connection established.");
+
+    console.log("Fetching properties from the database...");
     const properties = await propertyModel.find();
-    return NextResponse.json(properties);
+    console.log(`Found ${properties.length} properties.`);
+
+    const response = NextResponse.json(properties);
+    return addCorsHeaders(response);
   } catch (error) {
-    console.log(error);
+    console.log("Error occurred during GET request:", error);
     return NextResponse.json(
       { message: "Error fetching properties" },
       { status: 500 }
